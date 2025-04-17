8d9fe722dc6cce66e5d59e4d8c8775c0672a32a6
Mikita Slabysh <slabysh2015@gmail.com>
2025-04-17 02:28:48 -0400

    Started basic design of the app

--- JRestaurantSystem/Form1.Designer.cs
+++ JRestaurantSystem/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Location = new Point(-2, 74);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(585, 447);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Control;
+            button1.Font = new Font("Segoe UI", 25F);
+            button1.Location = new Point(936, 551);
+            button1.Name = "button1";
+            button1.Size = new Size(217, 55);
+            button1.TabIndex = 1;
+            button1.Text = "Log Out";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.SteelBlue;
+            flowLayoutPanel1.Location = new Point(578, 74);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(595, 447);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Home";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumBlue;
+            ClientSize = new Size(1173, 628);
+            Controls.Add(label1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "J's Restaurant Managment System";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
     }
 }
