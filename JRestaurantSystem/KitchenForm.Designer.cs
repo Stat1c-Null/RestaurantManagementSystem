1631a9868c6cce7d424da1aa417df1de27ffe49a
Mikita Slabysh <slabysh2015@gmail.com>
2025-04-20 21:31:26 -0400

    Kitchen form for the queue

---
+++ JRestaurantSystem/KitchenForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace JRestaurantSystem
+{
+    partial class KitchenForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox listBoxOrders;
+        private System.Windows.Forms.Button buttonServeNext;
+        private System.Windows.Forms.Label labelQueue;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            listBoxOrders = new ListBox();
+            buttonServeNext = new Button();
+            labelQueue = new Label();
+            SuspendLayout();
+            // 
+            // listBoxOrders
+            // 
+            listBoxOrders.FormattingEnabled = true;
+            listBoxOrders.Location = new Point(30, 50);
+            listBoxOrders.Name = "listBoxOrders";
+            listBoxOrders.Size = new Size(1388, 772);
+            listBoxOrders.TabIndex = 0;
+            // 
+            // buttonServeNext
+            // 
+            buttonServeNext.Location = new Point(30, 270);
+            buttonServeNext.Name = "buttonServeNext";
+            buttonServeNext.Size = new Size(150, 30);
+            buttonServeNext.TabIndex = 1;
+            buttonServeNext.Text = "Serve Next Order";
+            buttonServeNext.UseVisualStyleBackColor = true;
+            buttonServeNext.Click += buttonServeNext_Click;
+            // 
+            // labelQueue
+            // 
+            labelQueue.AutoSize = true;
+            labelQueue.Location = new Point(100, 200);
+            labelQueue.Name = "labelQueue";
+            labelQueue.Size = new Size(243, 48);
+            labelQueue.TabIndex = 2;
+            labelQueue.Text = "Orders Queue";
+            // 
+            // KitchenForm
+            // 
+            ClientSize = new Size(1784, 924);
+            Controls.Add(labelQueue);
+            Controls.Add(buttonServeNext);
+            Controls.Add(listBoxOrders);
+            Name = "KitchenForm";
+            Text = "Kitchen Orders";
+            Load += KitchenForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ JRestaurantSystem/KitchenForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JRestaurantSystem
+{
+    public partial class KitchenForm : Form
+    {
+        public KitchenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KitchenForm_Load(object sender, EventArgs e)
+        {
+            UpdateQueueDisplay();
+        }
+
+        private void UpdateQueueDisplay()
+        {
+            listBoxOrders.Items.Clear();
+            foreach (var order in Form1.OrderQueue)
+            {
+                listBoxOrders.Items.Add(order.ToString());
+            }
+        }
+
+        private void buttonServeNext_Click(object sender, EventArgs e)
+        {
+            if (Form1.OrderQueue.Count > 0)
+            {
+                var served = Form1.OrderQueue.Dequeue();
+                MessageBox.Show($"Served: {served}");
+                UpdateQueueDisplay();
+            }
+            else
+            {
+                MessageBox.Show("No orders to serve.");
+            }
+        }
+    }
+}
